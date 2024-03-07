21e3873ee05a23bcd6ff280c271050697eb2b9ed
賽博農民工阿溟 <WuSandWitch@gmail.com>
2024-03-07 14:50:15 +0800

    Upload template

--- app.py
+++ app.py
@@ -11,19 +11,19 @@ def main():
 
 @app.route("/get_ip",methods=["GET","POST"])
 def get_ip():
-    if request.method == "POST":
-        domain_name = request.form.get("domain_name")
-        data = os.popen(f"nslookup {domain_name}").read().split('\n')
-        return render_template("get_ip.html", data=data)
+    # if request.method == "POST":
+    #     domain_name = request.form.get("domain_name")
+    #     data = os.popen(f"nslookup {domain_name}").read().split('\n')
+    #     return render_template("get_ip.html", data=data)
     return render_template("get_ip.html")
 
 
 @app.route("/get_files",methods=["GET","POST"])
 def get_files():
-    if request.method == "POST":
-        directory = request.form.get("directory")
-        data = os.popen(f'ls directories/{directory}').read().split('\n')
-        return render_template("get_files.html", data=data)
+    # if request.method == "POST":
+    #     directory = request.form.get("directory")
+    #     data = os.popen(f'ls directories/{directory}').read().split('\n')
+    #     return render_template("get_files.html", data=data)
     return render_template("get_files.html")
 
 
